beb478b5958f7e14bf2e8b17caa066e92762f25e
Fran <fducekuce@gmail.com>
2024-07-07 18:41:10 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react"
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
+
+function App() {
+  const [todos, setTodos] = useState(["Eat", "Sleep", "Code", "Repeat"])
+  const [todoValue, setTodoValue] = useState("")
+
+  function persistData(newList) {
+    localStorage.setItem("todos", JSON.stringify({ todos: newList }))
+  }
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index
+    })
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
+  function handleEditTodo(index) {
+    setTodoValue(todos[index])
+    handleDeleteTodo(index)
+  }
+
+  useEffect(() => {
+    if (!localStorage) return
+    let localTodos = localStorage.getItem("todos")
+    if (!localTodos) return
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+  }, [])
+  return (
+    <>
+      <TodoInput
+        handleAddTodos={handleAddTodos}
+        todoValue={todoValue}
+        setTodoValue={setTodoValue}
+      />
+      <TodoList
+        todos={todos}
+        handleDeleteTodo={handleDeleteTodo}
+        handleEditTodo={handleEditTodo}
+      />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,21 @@
+export default function TodoInput(props) {
+  const { handleAddTodos, todoValue, setTodoValue } = props
+  return (
+    <header>
+      <input
+        value={todoValue}
+        onChange={(e) => setTodoValue(e.target.value)}
+        type="text"
+        placeholder="Enter here:"
+      />
+      <button
+        onClick={() => {
+          handleAddTodos(todoValue)
+          setTodoValue("")
+        }}
+      >
+        Add
+      </button>
+    </header>
+  )
+}
